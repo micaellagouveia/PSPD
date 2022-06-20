d0589b7b0c7f4bc9123517b06e063fa9cb2e7d67
micaellagouveia <micaella2212@gmail.com>
2022-06-20 13:02:15 -0300

    Connect, send and receive array

---
+++ Lab1/client.c
@@ -0,0 +1,65 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#define PORT 8080
+#define MAX 10
+
+float f_aleat() {
+    float random = rand()%MAX + 1;
+    return random;
+}
+
+int main(int argc, char const* argv[])
+{
+    int sock = 0, valread, client_fd;
+    struct sockaddr_in serv_addr;
+    char* hello = "Hello from client";
+    char buffer[1024] = { 0 };
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+ 
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+ 
+    // Convert IPv4 and IPv6 addresses from text to binary
+    // form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+ 
+    if ((client_fd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // Gerar vetor
+    float vetor[MAX];
+
+    for(int i = 0; i < MAX; i++){
+        vetor[i] = f_aleat();
+        float tmp = vetor[i];
+        printf("NÚMERO: %.2f\n", tmp);
+        vetor[i] = sqrt(tmp);
+        printf("NÚMERO COM RAIX: %.2f\n", vetor[i]);
+        
+        send(sock, &vetor[i], sizeof(float), 0);
+        printf("[Client]: Número: %.2f - ENVIADO\n", vetor[i]);
+    }
+
+
+
+    
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+ 
+    // closing the connected socket
+    close(client_fd);
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PSPD
+
+## Lab 1
+
+Comandos:
+* server: gcc server.c -o server && ./server
+* client: gcc client.c -o client -lm && ./client
